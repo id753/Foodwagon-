2ced0f7bf37e439a94144d7140dfb9701afbbc2c
Evgen <mimywhy@gmail.com>
2025-09-10 16:10:08 +0300

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  // Base public path when served in production.
-  // Make sure to replace 'your-repository-name' with the actual name of your GitHub repository.
-  base: "/Foodwagon-/",
-  plugins: [], // Any other plugins you might have
-});
